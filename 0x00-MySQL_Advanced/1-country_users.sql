7aa0c686082f16da29cbdc5c9d2634cdcf7ab31d
= <abdoahmedsayed704@gmail.com>
2024-10-27 11:02:51 +0300

    SQL PRACTICE

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- creates a table users
+-- Simple task i know
+
+CREATE TABLE IF NOT EXISTS users(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255),
+country ENUM ('US', 'CO', 'TN') NOT NULL
+);
